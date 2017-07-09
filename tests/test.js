541f96223b80e5d3a4b862f2e5deea35b26a9d38
Adam Coyne <github@mail2.coyne.nu>
2017-07-08 22:30:30 -0500

    Add tests

---
+++ tests/test.js
@@ -0,0 +1,21 @@
+var Shrimpit = require("../shrimpit");
+
+test("should be able to instantiate Shrimpit", () => {
+    expect(new Shrimpit()).toBeDefined();
+});
+
+test("test directory a", () => {
+    const shrimpit = new Shrimpit();
+    shrimpit.exec("./test/a");
+    expect(shrimpit.modules.exports).toMatchSnapshot("a exports");
+    expect(shrimpit.modules.imports).toMatchSnapshot("a imports");
+    expect(shrimpit.getUnresolved()).toMatchSnapshot("a unresolved");
+});
+
+test("test directory b", () => {
+    const shrimpit = new Shrimpit();
+    shrimpit.exec("./test/b");
+    expect(shrimpit.modules.exports).toMatchSnapshot("b exports");
+    expect(shrimpit.modules.imports).toMatchSnapshot("b imports");
+    expect(shrimpit.getUnresolved()).toMatchSnapshot("b unresolved");
+});
